7432ac4cf3f909647901070205e37a5e7a81fa99
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-02 19:58:49 +0500

    Array

--- Assignment1/src/test/java/ArrayInsertionTest.java
+++ Assignment1/src/test/java/ArrayInsertionTest.java
@@ -7,15 +7,13 @@ public class ArrayInsertionTest {
 
 	@Test
 	public void Array_Instest()
-	{
-		 // Array array = new Array(); //create an array.	
+	{	
 		    int [] array = new int[7];
 		    ArrayInsertion.insert(array,1,0);
 		    ArrayInsertion.insert(array,5,1);
 		    ArrayInsertion.insert(array,57,2);
 		    ArrayInsertion.insert(array,12,3);
 		    ArrayInsertion.insert(array,9,4);
-		 // array.insert(value,index);//insert 
 		    
 		  int[] expectedArray= {1,5,57,12,9,0,0};
 		  
@@ -27,14 +25,14 @@ public class ArrayInsertionTest {
 	@Test
 	public void Array_Instest_Fail()
 	{
-		 // Array array = new Array(); //create an array.	
+		 	//In this test case the error is occur because we we acces out of bound
 		    int [] ArrayF = new int[7];
 		    ArrayInsertion.insert(ArrayF,1,0);
 		    ArrayInsertion.insert(ArrayF,5,1);
 		    ArrayInsertion.insert(ArrayF,57,2);
 		    ArrayInsertion.insert(ArrayF,12,3);
 		    ArrayInsertion.insert(ArrayF,9,4);
-		 // array.insert(value,index);//insert 
+		 
 		    
 		  int[] expectedFArray= {1,5,57,12,9,0,0};
 		  
