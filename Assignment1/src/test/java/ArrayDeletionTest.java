a14c3d109a1deddcd695cc99208e6ce95a667c0a
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-02 19:55:03 +0500

    Array

--- Assignment1/src/test/java/ArrayDeletionTest.java
+++ Assignment1/src/test/java/ArrayDeletionTest.java
@@ -3,38 +3,45 @@ import org.junit.Test;
 public class ArrayDeletionTest {
 
 	@Test
-	 public void deleteTestPassing() 
+	 public void Pass_Test1() 
 	{
-        int[] array = {3, 4, 5, 11, 12, 13};
-        int deletionIndex = 3;
-        ArrayDeletion AB=new ArrayDeletion();
-        AB.delete(array,3);
-       // ArrayDeletion.AB.delete(array,3);
-        int[] expectedArray = {3, 4, 5, 12, 13, -1};
-        Assert.assertArrayEquals("Value at index 3 should be deleted and " + "original array should be affected", expectedArray, array);
+        int[] array = {1,2,3, 4, 5, 7};
+        int Index_Delete = 3;
+        ArrayDeletion objDelete=new ArrayDeletion();
+        objDelete.delete(array,3);
+       
+        int[] expectedArray = {1,2,3,5,7, -1};
+        Assert.assertArrayEquals("Value at index 3 deleted", expectedArray, array);
     }
 
-	
-    /*
-     * This test checks if the length of the array has reduced after
-     * performing the deletion. Here, as seen, the delete function
-     * doesn't alter the length of the resultant array and thereby
-     * it is bound to fail.
-     */
 	
     @Test
-    public void deleteTestFailing() {
-        int[] array = {3, 4, 5, 11, 12, 13};
-        int deletionIndex = 3;
-       // int expectedArrayLength = array.length;
-        ArrayDeletion AB=new ArrayDeletion();
-        AB.delete(array,3);
-        int[] expectedArray = {3, 4, 5, 12, 0, -1};
-       // ArrayDeletion.delete(array,3);
-        //Assert.assertEquals("Array size should reduce by 1 upon deletion",
-          //      expectedArrayLength, array.length);
-        Assert.assertArrayEquals("Value at index 3 should be deleted and " + "original array should be affected", expectedArray, array);
+    public void Pass_Test2() 
+    {
+        int[] array = {1,2,3, 4, 5, 7};
+        int Index_Delete = 4;
+       
+        ArrayDeletion objDelete=new ArrayDeletion();
+        objDelete.delete(array,4);
+        int[] expectedArray = {1,2,3, 4, 7, -1};
+        
+        Assert.assertArrayEquals("Value of index delete 4 ", expectedArray, array);
     }
-
+    
+    @Test
+    public void Pass_Test3() 
+    {
+    	//this test case pass because we check not equal
+        int[] array = {1,2,3, 4, 5, 7};
+        int Index_Delete = 5;
+       
+        ArrayDeletion objDelete=new ArrayDeletion();
+        objDelete.delete(array,5);
+        int[] expectedArray = {1,2,3, 4, 7, -1};
+        
+        Assert.assertNotEquals("Value of index 5 delete  ", expectedArray, array);
+    }
+ 
+    
 	
 }
