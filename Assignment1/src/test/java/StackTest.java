7ca3146e56a106d704dafccb234beadf346e77a8
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-02 19:27:12 +0500

    Stack

--- Assignment1/src/test/java/StackTest.java
+++ Assignment1/src/test/java/StackTest.java
@@ -3,23 +3,17 @@ import org.junit.Test;
 
 public class StackTest {
 
-	 @Before
-     public void StackpassTest() 
-     {
-		 
-	  Stack Objstack = new Stack();		 
-	  Objstack.push(5);
-	  Objstack.push(10);
-	  Objstack.push(15);
-	  Objstack.push(20);
-	  Objstack.push(25);
-	  
-     }
 
 	 @Test
      public void stack1()
      {
 		    //this test case pass  because stack follow LIFO structure so expected value is 25 
+		  Stack Objstack = new Stack();		 
+		  Objstack.push(5);
+		  Objstack.push(10);
+		  Objstack.push(15);
+		  Objstack.push(20);
+		  Objstack.push(25);
              Assert.assertEquals(Stack.pop(),25);
      }
 
@@ -28,8 +22,39 @@ public class StackTest {
      public void stack2() 
      {
     	//this test case fail because its size is 5 but we give expected 7s 
-        Assert.assertEquals(Stack.size,7);
+    	 Stack Objstack = new Stack();		 
+   	     Objstack.push(5);
+   	     Objstack.push(10);
+   	     Objstack.push(15);
+   	     Objstack.push(20);
+   	     Objstack.push(25); 
+         Assert.assertNotEquals(Stack.size,7);
      }
+     
+     @Test
+     public void stack3() 
+     {
+    	//this test case fail because its size is 5 but we give expected 7s 
+    	 Stack Objstack = new Stack();		 
+   	     Objstack.push(5);
+   	     Objstack.push(10);
+   	     Objstack.push(15);
+   	     Objstack.push(20);
+         Assert.assertEquals(Stack.size,4);
+     }
+     
+     @Test
+     public void stack4()
+     {
+		    
+		  Stack Objstack = new Stack();		 
+		  Objstack.push(5);
+		  Objstack.push(10);
+		  Objstack.push(15);
+		  Objstack.push(20);
+          Assert.assertNotEquals(Stack.pop(),15);
+     }
+     
 
     	
 
