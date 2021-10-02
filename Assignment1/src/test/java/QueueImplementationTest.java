09af616cd9968d3d6720abfeae676ce1871e4f9d
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-02 18:57:52 +0500

    Queue

--- Assignment1/src/test/java/QueueImplementationTest.java
+++ Assignment1/src/test/java/QueueImplementationTest.java
@@ -14,7 +14,7 @@ public class QueueImplementationTest
 		OBJ.enqueue(4);
 		OBJ.enqueue(5);
 		
-	 	Assert.assertEquals(OBJ.dequeue(), 3);
+	 	Assert.assertEquals(OBJ.dequeue(),3);
 	 	
 	}
 	
@@ -22,6 +22,7 @@ public class QueueImplementationTest
 	@Test
 	public void Quetest2() throws Exception 
 	{
+		//test case pass because we check false
 		OBJ.enqueue(9);
 	 	Assert.assertFalse(OBJ.empty() == true);
 	 	
@@ -33,9 +34,34 @@ public class QueueImplementationTest
 	public void Quetest3() throws Exception 
 	{
 		OBJ.enqueue(7);
-		Assert.assertEquals(OBJ.dequeue(), 7);
+		OBJ.enqueue(3);
+		OBJ.enqueue(4);
+		OBJ.enqueue(5);
+
+		Assert.assertEquals(OBJ.dequeue(),7);
 	 	
 	}
+	
+	@Test
+	public void Quetest4() throws Exception 
+	{
+		//test case pass because we check not equal
+		OBJ.enqueue(5);
+		OBJ.enqueue(6);
+		OBJ.enqueue(7);
+		OBJ.enqueue(8);
+
+		Assert.assertNotEquals(OBJ.dequeue(),6);
+		
+	}
+	
+	@Test
+	public void Quetest5() throws Exception 
+	{
+	 	Assert.assertTrue(OBJ.empty() == true);
+	 	
+	}
+
 
 	
 }
