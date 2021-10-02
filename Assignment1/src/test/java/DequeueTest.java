a254f4e2916c09b98d0463d9076b78518f381d29
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-02 18:50:01 +0500

    Queue

--- Assignment1/src/test/java/DequeueTest.java
+++ Assignment1/src/test/java/DequeueTest.java
@@ -3,35 +3,38 @@ import org.junit.Test;
 
 public class DequeueTest
 {	 
-	   //objects of Dequeue class
-	
+	   
 	   Dequeue ObjQue1 = new Dequeue(15);	  
 	   Dequeue ObjQue2 = new Dequeue(15);
 	  
 	   @Test
-	   public void Fail_Que_test() 
+	   public void Que_test1() 
 	   {   
+		   //this test case pass because we check	not same
 		   ObjQue1.addLeft(30);
 		   ObjQue1.addRight(40);
 		   ObjQue2.addLeft(50);
 		   ObjQue2.addRight(60);
 		  
-	       Assert.assertSame(ObjQue2, ObjQue1);
+	       Assert.assertNotSame(ObjQue2, ObjQue1);
 	      
 	   }
-
+	   
+	   
 	   
 	   @Test
-	   public void Pass_Que_test() 
+	   public void Pass_Que_test2() 
 	   {
 		   ObjQue1.addLeft(30);
 		   ObjQue1.addRight(15);
 	       Assert.assertNotNull(ObjQue1); 
 	       
 	   }
+	   
+	   
 	  
 	   @Test
-	   public void Pass_Que_test2() 
+	   public void Pass_Que_test3() 
 	   {   
 		   ObjQue1.addLeft(10);
 		   ObjQue1.addRight(50);
@@ -41,5 +44,31 @@ public class DequeueTest
 	       Assert.assertNotSame(ObjQue2, ObjQue1);
 	      
 	   }
+	   
+	   @Test
+	   public void Que_test4() 
+	   {   
+		   //this test case pass because we check	not same
+		   ObjQue1.addLeft(-30);
+		   ObjQue1.addRight(-40);
+		   ObjQue2.addLeft(-20);
+		   ObjQue2.addRight(-10);
+		  
+	       Assert.assertNotEquals(ObjQue2, ObjQue1);
+	      
+	   }
+	   
+	   @Test
+	   public void Pass_Que_test5() 
+	   {
+		   
+		   ObjQue1.addLeft(30);
+		   ObjQue1.addRight(15);
+		   ObjQue2.addLeft(10);
+		   ObjQue2.addRight(5);
+	       Assert.assertNotEquals(ObjQue2, ObjQue1);
+	       
+	   }
+
 	  
 }
